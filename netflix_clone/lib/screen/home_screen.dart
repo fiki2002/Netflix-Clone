7424c77cdb84b7ff3d106dca639f45ce6c065c69
fiki2002 <adepitanoluwatosin202@gmail.com>
2022-09-13 22:53:44 +0100

    Implemented Scroll Controller and its offset

--- netflix_clone/lib/screen/home_screen.dart
+++ netflix_clone/lib/screen/home_screen.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 
 import '../widgets/widgets.dart';
 
-
-
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -12,19 +10,59 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  double _scrollOffset = 0.0;
+  late ScrollController _scrollController;
+  @override
+  void initState() {
+    _scrollController = ScrollController()
+      ..addListener(
+        () {
+          setState(() {
+            _scrollOffset = _scrollController.offset;
+          });
+        },
+      );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final Size screenSize = MediaQuery.of(context).size;
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          debugPrint('cast');
+        },
         backgroundColor: Colors.grey[850],
         child: const Icon(
           Icons.cast,
         ),
       ),
-      appBar: PreferredSize(preferredSize: Size(screenSize.width, 50.0,),
-      child: const CustomAppBar(),
+      appBar: PreferredSize(
+        preferredSize: Size(
+          screenSize.width,
+          50.0,
+        ),
+        child: CustomAppBar(
+          scrollOffset: _scrollOffset,
+        ),
+      ),
+      body: CustomScrollView(
+        controller: _scrollController,
+        slivers: [
+          SliverToBoxAdapter(
+            child: Container(
+              height: 1000,
+              color: Colors.blue,
+            ),
+          ),
+        ],
       ),
     );
   }
--- netflix_clone/lib/widgets/custom_app_bar.dart
+++ netflix_clone/lib/widgets/custom_app_bar.dart
@@ -3,8 +3,13 @@ import 'package:flutter/material.dart';
 import '../assets.dart';
 
 class CustomAppBar extends StatelessWidget {
-  const CustomAppBar({Key? key}) : super(key: key);
+  const CustomAppBar({
+    Key? key,
+    this.scrollOffset = 0.0,
+  }) : super(key: key);
 
+  final double scrollOffset;
+  
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -12,13 +17,59 @@ class CustomAppBar extends StatelessWidget {
         vertical: 10,
         horizontal: 24,
       ),
-      color: Colors.orange,
+      color: Colors.black.withOpacity(
+        (scrollOffset / 350).clamp(0,1).toDouble(),
+      ),
       child: Row(
         children: [
           Image.asset(
             Assets.netflixLogo0,
           ),
+          const SizedBox(
+            width: 12,
+          ),
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                _AppBarButton(
+                  text: 'TV Shows',
+                  onTap: () => debugPrint('TV Shows'),
+                ),
+                _AppBarButton(
+                  text: 'Movies',
+                  onTap: () => debugPrint('Movies'),
+                ),
+                _AppBarButton(
+                  text: 'My Lists',
+                  onTap: () => debugPrint('My Lists'),
+                ),
+              ],
+            ),
+          ),
         ],
+      ),
+    );
+  }
+}
+
+class _AppBarButton extends StatelessWidget {
+  const _AppBarButton({Key? key, required this.text, required this.onTap})
+      : super(key: key);
+  final String text;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Text(
+        text,
+        style: const TextStyle(
+          color: Colors.white,
+          fontSize: 16,
+          fontWeight: FontWeight.w600,
+        ),
       ),
     );
   }
