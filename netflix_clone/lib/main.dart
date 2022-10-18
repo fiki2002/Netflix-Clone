aaf234f34f529e8cd9db349a3242ab1be3d6955b
fiki2002 <adepitanoluwatosin202@gmail.com>
2022-10-18 18:22:56 +0100

    Fixed Provider issue

--- netflix_clone/lib/main.dart
+++ netflix_clone/lib/main.dart
@@ -6,10 +6,9 @@ import 'package:netflix_clone/provider/appbar_provider.dart';
 import 'package:netflix_clone/screen/screens.dart';
 
 void main() {
-  runApp( DevicePreview(
-    enabled: true,
-    builder: (context) =>  const MyApp(),
-  ), );
+  runApp(
+    const MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -17,20 +16,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-   return ChangeNotifierProvider<AppBarProvider>(
-    create: (BuildContext context) => AppBarProvider(),
-     child: MaterialApp(
-      title: 'Flutter Netflix UI',
-      useInheritedMediaQuery: true,
-      locale: DevicePreview.locale(context),
-      builder: DevicePreview.appBuilder,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        scaffoldBackgroundColor: Colors.black,
+    return ChangeNotifierProvider<AppBarProvider>(
+      create: (BuildContext context) => AppBarProvider(),
+      child: MaterialApp(
+        title: 'Flutter Netflix UI',
+        useInheritedMediaQuery: true,
+        locale: DevicePreview.locale(context),
+        builder: DevicePreview.appBuilder,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          scaffoldBackgroundColor: Colors.black,
+        ),
+        home: const NavScreen(),
       ),
-      home: const NavScreen(),
-    ),
-   );
+    );
   }
 }
